c152508037a546bbcd8936b1e443d7ed7af2df0d
YAndoRak <40165855+YAndoRak@users.noreply.github.com>
2020-08-07 10:09:45 +0300

    add access token

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ app.use(bodyParser.urlencoded({
     extended:false
 }));
 
-const token = ''
+const token = 'EAAIiXXZBZBZAd8BAB4j1XmmTR0nxe1V1n3LcJYZB6HmSoTbEwCURZBrqu4SMj9a9JZBSSiI316IZBK9yyDWYM9gzDXRUe73jvYdSbzzfcaruDvaNyCuHymK8ZCnVFcpOdArZAJZCWpgVtYcCW7jgTYMgFTZCZCNKStwO8wIdZCF9EqFxirQjtdbJS9G0X'
 
 function launchDown(){
   request({
     url:'https://graph.facebook.com/v7.0/109923754135421/videos',
     qs:{access_token: token},
-    method:'POST'?
+    method:'POST',
     json:{
      is_reusable;'true',
      file_url:'https://techslides.com/demos/sample-videos/small.mp4'
