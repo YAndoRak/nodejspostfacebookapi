30fb75e00c8390e7809be22f2f0039788aea8d9b
ANDRIAMIALY Sarobidy <sarobidy.antonio.andriamialy@esti.mg>
2020-08-29 01:45:43 +0300

    modify hoolla bot token

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ app.use(bodyParser.urlencoded({
     extended:false
 }));
 
-const token = 'EAAIiXXZBZBZAd8BAB4j1XmmTR0nxe1V1n3LcJYZB6HmSoTbEwCURZBrqu4SMj9a9JZBSSiI316IZBK9yyDWYM9gzDXRUe73jvYdSbzzfcaruDvaNyCuHymK8ZCnVFcpOdArZAJZCWpgVtYcCW7jgTYMgFTZCZCNKStwO8wIdZCF9EqFxirQjtdbJS9G0X'
+//const token = 'EAAIiXXZBZBZAd8BAB4j1XmmTR0nxe1V1n3LcJYZB6HmSoTbEwCURZBrqu4SMj9a9JZBSSiI316IZBK9yyDWYM9gzDXRUe73jvYdSbzzfcaruDvaNyCuHymK8ZCnVFcpOdArZAJZCWpgVtYcCW7jgTYMgFTZCZCNKStwO8wIdZCF9EqFxirQjtdbJS9G0X'
+
+const token = 'EAAI1QygXjocBAJupxEXFiNJAFlIGCA44EjIe1Itui35dZCfTZAZCZAUiZBYCRcbs6X4sDhOq5IKm6OJK16i9hqw7d3podv0717ezpMwiDspFfaYiH1vsyZBDIvgYtLOa6dca2GSPv9RuD2R0Be7CW29jaBrde85y3kX8OSDwzmZBc3ptnKgo20y'
 app.get('/:ytbId/:tagId', function (req, res) {
   console.log(req.params.tagId);
   //http://localhost:8000/fr/123
